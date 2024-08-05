4c70e779865fe982134c9b8559e897892728c9d1
Abdelrahman Abdelmoaty <abdelr7manabdelmoaty@gmail.com>
2024-08-06 00:59:02 +0300

    updates

--- resources/views/course/partials/sidebar.blade.php
+++ resources/views/course/partials/sidebar.blade.php
@@ -3,8 +3,11 @@
     <button
         class="fixed top-0 right-0 z-50 inline-flex items-center justify-center p-4 transition-opacity duration-150 ease-in-out rounded-md lg:hidden hover:opacity-85"
         @click="open=!open">
-        <svg class="w-6 h-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+        <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" viewBox="0 0 24 24" fill="none"
+            stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+            class="lucide lucide-square-play">
+            <rect width="18" height="18" x="3" y="3" rx="2" />
+            <path d="m9 8 6 4-6 4Z" />
         </svg>
     </button>
 
@@ -95,7 +98,7 @@
                                     '!border-l-primary-dark dark:!border-l-primary bg-gray-200 dark:bg-gray-800': episodeId ==
                                         `{{ $chapterEpisode->id }}`,
                                 }"
-                                @click="episodeId = '{{ $chapterEpisode->id }}'; chapterId = '{{ $courseChapter->id }}'; activeEpisode = {{ json_encode($chapterEpisode) }}; activeChapter = {{ json_encode($courseChapter) }}; history.pushState({}, '', `/episode/{{ $chapterEpisode->uuid }}`)">
+                                @click="episodeId = '{{ $chapterEpisode->id }}'; chapterId = '{{ $courseChapter->id }}'; activeEpisode = {{ json_encode($chapterEpisode) }}; activeChapter = {{ json_encode($courseChapter) }}; open=false; history.pushState({}, '', `/episode/{{ $chapterEpisode->uuid }}`)">
 
                                 <svg :class="{
                                     'hidden': {{ $chapterEpisode->is_seen }},
